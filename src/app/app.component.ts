edca9845842a6e615a4f5643a9bfa993b052ebd2
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-22 18:20:11 -0300

    add get posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "./../environments/environment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
+      .post(`${environment.apiURL}posts.json`, postData)
+      .subscribe((responseData) => {
         console.log(responseData);
       });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(`${environment.apiURL}posts.json`)
+      .subscribe((posts) => console.log(posts));
   }
 }
