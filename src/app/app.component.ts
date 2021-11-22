54ae37987537e9dd44bd7b79e829e7cfc04aa940
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-22 18:43:40 -0300

    add post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "./../environments/environment";
+import { map } from "rxjs/operators";
+import Post from "./post.model";
 
 @Component({
   selector: "app-root",
@@ -16,7 +18,7 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
       .post(`${environment.apiURL}posts.json`, postData)
@@ -36,6 +38,18 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http
       .get(`${environment.apiURL}posts.json`)
+      .pipe(
+        map((responseData: { [key: string]: Post }) => {
+          const postsArray: Post[] = [];
+
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
       .subscribe((posts) => console.log(posts));
   }
 }
---
+++ src/app/post.model.ts
@@ -0,0 +1,5 @@
+export default interface Post {
+  tile: string;
+  content: string;
+  id?: string;
+}
